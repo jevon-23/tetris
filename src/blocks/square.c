1cf07c10b9ccbf1fe2721e2eda470d49791d6da3
jevon-23 <jevon32101@gmail.com>
2022-07-19 01:36:29 -0400

    basic square block place fn

--- src/blocks/square.c
+++ src/blocks/square.c
@@ -1,15 +1,83 @@
+#include "../../include/square.h"
+#include "../../include/game_board.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <float.h>
-#include <stdbool.h>
 
-int processCLI(int argc, char **argv, char **filename) {
-  if (argc != 2) {
-    printf("usage:");
+/* Checks for collisions in direction that we are trying to move in */
+bool collision(game_board *board, square_block *s, int dir, int row, int col);
+
+/* Does not rotate in any way b/c it is a square */
+void rotate_square_block(game_board *board, square_block *s, bool direct) {
+  return;
+}
+
+square_block *make_square() {
+  square_block *out = (square_block *)malloc(sizeof(square_block));
+  out->typ = SQUARE;
+  out->dir = 0;
+  out->dim = make_dimensions(2, 2); /* 2 x 2 */
+  out->row_pos = 0;
+  out->col_pos = ROWS / 2;
+
+  /* Functions */
+  out->move_block = move_square;
+  out->rotate_block = rotate_square_block;
+  out->place_block = place_square;
+  return out;
+}
+
+block *move_square(game_board *board, square_block *s, int row, int col) {
+  return NULL;
+}
+
+/* dir == what way are we moving */
+bool square_collision(game_board *board, square_block *s, int dir, int row,
+                      int col) {
+
+  switch (dir) {
+
+  case 0:
+    for (int i = 0; i < s->dim.rows; i++) {
+      block *b = get_block(board, row + i, col);
+      block *bl = get_block(board, row + i, col + 1);
+      if (b->typ != EMPTY || bl->typ != EMPTY)
+        return true;
+    }
+    break;
+
+  default:
+    printf("invalid collision direction: %d\n", dir);
     exit(-1);
   }
-  return 0;
+  return false;
 }
-int main(int arg, char *argv[]) {
+
+bool place_square(game_board *board, square_block *s, int row, int col) {
+
+  /* Create a new square block, that is set 1 unit below current one */
+  square_block *new_block = make_square();
+
+  /* Check to see if we are placing a block out of bounds */
+  if (col + s->dim.cols > COLS)
+    col = COLS - s->dim.cols;
+  if (col < 0)
+    col = 0;
+  if (row < 0)
+    row = 0;
+
+  if (square_collision(board, s, 0, row, col))
+    return false;
+  for (int i = 0; i < s->dim.rows; i++) {
+    /* Place the value on the board */
+    *(*(board->board + i + row) + col) = *s;
+    *(*(board->board + i + row) + col - 1) = *s;
+
+    /* Update the group to have the right blocks */
+    s->group[i].rows = i + row;
+    s->group[i].cols = col;
+  }
+
+  return new_block;
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*  
+/*
  * Set up a little set up screen where we ask the user if they want
  * to play or if they watch the ai play, whenever we make that
  */
@@ -22,65 +22,65 @@ int main(int arg, char *argv[]) {
   /* Generate this games' board */
   game_board *board = make_game_board();
   printf("made the game_board\n");
-  block *next = make_block(LINE);
+  block *next = make_block(SQUARE);
   block *prev = malloc(sizeof(block));
   *prev = *next;
 
-  place_block(board, next, 0, COLS/2);
+  place_block(board, next, 0, COLS / 2);
+  print_game_board(board);
+  place_block(board, next, 0, COLS / 2);
   print_game_board(board);
 
-
-  for (int i = 0; i < 12; i++) {
-  while (next != NULL) {
-    next = move_block_down(board, (block *)next);
-    print_game_board(board);
-    if (next == NULL) break;
-    *prev = *next;
-
-    next = move_block_left(board, next);
-    print_game_board(board);
-    if (next == NULL){
-      next = malloc(sizeof(block));
-      *next = *prev;
-    }
-    *prev = *next;
-
-    next = move_block_right(board, next);
-    print_game_board(board);
-    if (next == NULL){
-      *next = *prev;
-    }
-    *prev = *next;
-
-    rotate_block(board, next);
-    print_game_board(board);
-  }
-  prev->active = false;
-
-  next = make_block(LINE);
-  if (place_block(board, next, 0, COLS/2) == false)
-      break;
-  if (i % 2 == 0){
-
-    next = move_block_right(board, next);
-    next = move_block_right(board, next);
-  }
-  if (i % 3 == 0) {
-    next = move_block_left(board, next);
-    next = move_block_left(board, next);
-    next = move_block_left(board, next);
-  }
-  if (i % 4 == 0) {
-    next = move_block_left(board, next);
-    next = move_block_left(board, next);
-  }
-  if (i % 5 == 0) {
-    next = move_block_right(board, next);
-    next = move_block_right(board, next);
-  }
-    print_game_board(board);
-}
-
+  //  for (int i = 0; i < 12; i++) {
+  //  while (next != NULL) {
+  //    next = move_block_down(board, (block *)next);
+  //    print_game_board(board);
+  //    if (next == NULL) break;
+  //    *prev = *next;
+  //
+  //    next = move_block_left(board, next);
+  //    print_game_board(board);
+  //    if (next == NULL){
+  //      next = malloc(sizeof(block));
+  //      *next = *prev;
+  //    }
+  //    *prev = *next;
+  //
+  //    next = move_block_right(board, next);
+  //    print_game_board(board);
+  //    if (next == NULL){
+  //      *next = *prev;
+  //    }
+  //    *prev = *next;
+  //
+  //    rotate_block(board, next);
+  //    print_game_board(board);
+  //  }
+  //  prev->active = false;
+  //
+  //  next = make_block(LINE);
+  //  if (place_block(board, next, 0, COLS/2) == false)
+  //      break;
+  //  if (i % 2 == 0){
+  //
+  //    next = move_block_right(board, next);
+  //    next = move_block_right(board, next);
+  //  }
+  //  if (i % 3 == 0) {
+  //    next = move_block_left(board, next);
+  //    next = move_block_left(board, next);
+  //    next = move_block_left(board, next);
+  //  }
+  //  if (i % 4 == 0) {
+  //    next = move_block_left(board, next);
+  //    next = move_block_left(board, next);
+  //  }
+  //  if (i % 5 == 0) {
+  //    next = move_block_right(board, next);
+  //    next = move_block_right(board, next);
+  //  }
+  //    print_game_board(board);
+  //}
 
   printf("Successful build!\n");
   return 0;
