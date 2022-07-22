63a0a1644dcbe3de1da87635f03e726ce4943164
jevon-23 <jevon32101@gmail.com>
2022-07-21 21:19:03 -0400

    more square, almost ok

--- src/blocks/square.c
+++ src/blocks/square.c
@@ -28,10 +28,6 @@ square_block *make_square() {
   return out;
 }
 
-block *move_square(game_board *board, square_block *s, int row, int col) {
-  return NULL;
-}
-
 /* dir == what way are we moving */
 bool square_collision(game_board *board, square_block *s, int dir, int row,
                       int col) {
@@ -39,9 +35,10 @@ bool square_collision(game_board *board, square_block *s, int dir, int row,
   switch (dir) {
 
   case 0:
+    /* Moving down */
     for (int i = 0; i < s->dim.rows; i++) {
       block *b = get_block(board, row + i, col);
-      block *bl = get_block(board, row + i, col + 1);
+      block *bl = get_block(board, row + i, col - 1);
       if (b->typ != EMPTY || bl->typ != EMPTY)
         return true;
     }
@@ -54,10 +51,47 @@ bool square_collision(game_board *board, square_block *s, int dir, int row,
   return false;
 }
 
-bool place_square(game_board *board, square_block *s, int row, int col) {
+block *move_square(game_board *board, square_block *s, int row, int col) {
+  /* Check for collision */
 
-  /* Create a new square block, that is set 1 unit below current one */
-  square_block *new_block = make_square();
+  if (row > s->row_pos) {
+    /* Check for collision below */
+    for (int i = 0; i < s->dim.cols; i++) {
+      block *lower_block =
+          get_block(board, s->row_pos + s->dim.rows, s->col_pos + i);
+      if (lower_block == NULL)
+        continue;
+      if (lower_block->typ != EMPTY) {
+        return NULL;
+      }
+    }
+  }
+
+  /* Check for collision on sides */
+  if (col < s->col_pos) {
+    for (int i = 0; i < s->dim.rows; i++) {
+      block *lower_block = get_block(board, row + i, col - (s->col_pos - col));
+      if (lower_block == NULL)
+        continue;
+      if (lower_block->typ != EMPTY) {
+        return NULL;
+      }
+    }
+  }
+  /* Make a new square block, and store it */
+  square_block *new = (square_block *)make_block(SQUARE);
+
+  for (; new->dir != s->dir % 2; rotate_square_block(board, new, true))
+    ;
+
+  /* Free previous block group */
+  free_block_group(board, (block *)s);
+  place_block(board, new, row, col);
+
+  return new;
+}
+
+bool place_square(game_board *board, square_block *s, int row, int col) {
 
   /* Check to see if we are placing a block out of bounds */
   if (col + s->dim.cols > COLS)
@@ -75,9 +109,11 @@ bool place_square(game_board *board, square_block *s, int row, int col) {
     *(*(board->board + i + row) + col - 1) = *s;
 
     /* Update the group to have the right blocks */
-    s->group[i].rows = i + row;
-    s->group[i].cols = col;
+    s->group[2 * i].rows = i + row;
+    s->group[2 * i].cols = col;
+    s->group[2 * i + 1].rows = i + row;
+    s->group[2 * i + 1].cols = col - 1;
   }
 
-  return new_block;
+  return s;
 }
